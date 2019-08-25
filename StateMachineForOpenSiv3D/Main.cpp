825f078ff903086273db83f237fd664ca0e24d7b
mattyan1053 <h.matsumaruka14161@gmail.com>
2019-08-25 16:27:50 +0900

    ステートマシン実装

---
+++ StateMachineForOpenSiv3D/Main.cpp
@@ -0,0 +1,104 @@
+﻿
+# include <Siv3D.hpp> // OpenSiv3D v0.4.0
+
+# include "StateMachine.hpp"
+
+// 状態
+// 列挙体を使っているがStringなどでも良い
+enum BallStates {
+	Idle,
+	Moving,
+};
+
+// 状態管理したいクラス
+class MovingBall : public StateMachine<BallStates> {
+
+	Circle m_ball;
+
+public:
+
+	MovingBall() {
+		m_ball.r = 40;
+		m_ball.setPos({ 100, 200 });
+		initializeStateMachine();
+		goToState(BallStates::Idle);
+	}
+
+	virtual void initializeStateMachine() override {
+		// idle
+		{
+			auto s = new Idle(*this);
+			addState(s);
+		}
+
+		// moving
+		{
+			auto s = new Moving(*this);
+			addState(s);
+		}
+	}
+
+	// 状態をインナークラスで定義する。
+	struct Idle : public State<BallStates> {
+		MovingBall& main;
+		Idle(MovingBall &_main) :State<BallStates>(BallStates::Idle), main(_main) {}
+
+		virtual void setUp() override {
+			main.m_ball.x = 100;
+			Print << U"Idle";
+		}
+		virtual void update() override {
+			if (main.m_ball.leftClicked()) {
+				main.goToState(BallStates::Moving);
+			}
+		}
+		virtual void draw() const override {
+			main.m_ball.draw(Palette::Blue);
+		}
+		virtual void cleanUp() override {
+			Print << U"Start moving";
+		}
+	};
+
+	struct Moving : public State<BallStates> {
+		MovingBall& main;
+		Moving(MovingBall& _main) :State<BallStates>(BallStates::Moving), main(_main) {}
+
+		virtual void setUp() override {
+			Print << U"Moving";
+		}
+		virtual void update() override {
+			main.m_ball.x += 10;
+			if (main.m_ball.x >= 500) {
+				main.goToState(BallStates::Idle);
+			}
+		}
+		virtual void draw() const override {
+			main.m_ball.draw(Palette::Red);
+		}
+		virtual void cleanUp() override {
+			Print << U"Finish Moving";
+		}
+	};
+
+};
+
+void Main()
+{
+	// 背景を水色にする
+	Scene::SetBackground(ColorF(0.8, 0.9, 1.0));
+
+	// 大きさ 60 のフォントを用意
+	const Font font(60);
+
+	// 状態を持つオブジェクト
+	MovingBall b;
+
+	while (System::Update())
+	{
+
+		b.update();
+		b.draw();
+
+	}
+}
---
+++ StateMachineForOpenSiv3D/StateMachine.hpp
@@ -0,0 +1,150 @@
+# pragma once
+# include <unordered_map>
+# include <Siv3D.hpp>
+
+/// <summary>
+/// 状態基底クラス
+/// </summary>
+template<typename T>
+class State {
+private:
+
+	T m_id;
+
+public:
+
+	/// <summary>
+	/// 新しい状態を作成します。
+	/// </summary>
+	/// <param name="id"> 状態ID </param>
+	State(T id) :m_id(id) {}
+
+	/// <summary>
+	/// 状態IDを取得します。
+	/// </summary>
+	T Id() { return m_id; }
+
+	/// <summary>
+	/// 状態に入ったときに呼ばれます。
+	/// </summary>
+	virtual void setUp() = 0;
+
+	/// <summary>
+	/// 毎フレーム呼ばれます。情報を更新します。
+	/// </summary>
+	virtual void update() = 0;
+
+	/// <summary>
+	/// 毎フレーム呼ばれます。描画を更新します。
+	/// </sumamry>
+	virtual void draw() const = 0;
+
+	/// <summary>
+	/// 次の状態に移る前に呼ばれます。
+	/// </summary>
+	virtual void cleanUp() = 0;
+
+};
+
+/// <summary>
+/// 状態管理クラス
+/// </summary>
+template<typename T>
+class StateMachine {
+
+private:
+
+	/// <summary>
+	/// 状態リスト
+	/// </summary>
+	std::unordered_map<T, std::shared_ptr<State<T>>> stateList;
+
+	/// <summary>
+	/// 現在の状態
+	/// </summary>
+	std::shared_ptr<State<T>> state;
+
+public:
+
+	/// <summary>
+	/// デフォルトコンストラクタ
+	/// </summary>
+	StateMachine() {	}
+
+	/// <summary>
+	/// 現在の状態を返します。
+	/// </summary>
+	/// <returns> 現在の状態 </returns>
+	State<T> currentState() {
+		return *state;
+	}
+
+	/// <summary>
+	/// 指定の状態へ移行します。
+	/// </summary>
+	/// <param name="nextStateId"> 移行先の状態ID </param>
+	void goToState(T nextStateId) {
+
+		if (!stateList.contains(nextStateId)) {
+			Print << U"Error: Not exist stete: " << nextStateId;
+			return;
+		}
+
+		if (state != nullptr) {
+			state->cleanUp();
+		}
+
+		state = stateList[nextStateId];
+		state->setUp();
+
+	}
+
+	/// <summary>
+	/// ステートマシンの初期化を行います。
+	/// </summary>
+	virtual void initializeStateMachine() = 0;
+
+	/// <summary>
+	/// 状態を追加します。
+	/// </summary>
+	void addState(State<T> *s) {
+		if (s == nullptr) {
+			Print << U"Error: This state is nullptr";
+			return;
+		}
+		if (stateList.contains(s->Id())) {
+			Print << U"Error: Already exist state: " << s->Id();
+			return;
+		}
+		stateList[s->Id()] = std::shared_ptr<State<T>>(s);
+	}
+
+	/// <summary>
+	/// 現在の状態が指定の状態かどうか調べます。
+	/// </summary>
+	/// <param name="stateId"> 現在の状態と比較する状態 </param>
+	/// <returns> 現在の状態が指定の状態と一致するかどうかの真偽 </returns>
+	bool isStateSame(T stateId) {
+		if (state == nullptr) return false;
+		return state->Id() == stateId;
+	}
+
+	/// <summary>
+	/// 毎フレーム呼ばれます。情報を更新します。
+	/// </summary>
+	void update() {
+		if (state != nullptr) {
+			state->update();
+		}
+	}
+
+	/// <summary>
+	/// 毎フレーム呼ばれます。描画を更新します。
+	/// </summary>
+	void draw() const {
+		if (state != nullptr) {
+			state->draw();
+		}
+	}
+
+};
